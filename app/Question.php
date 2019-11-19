7c15e3ec0722802c93c646296cefb3f1444fb915
Pit <pit@Pits-iMac.local>
2019-11-19 23:26:39 +0300

    add author info an question creation date item

--- app/Question.php
+++ app/Question.php
@@ -17,6 +17,16 @@ class Question extends Model
         $this->attributes['slug'] = str::slug($value);
     }
 
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
 //    $question = Question::find(1);
 
 }
