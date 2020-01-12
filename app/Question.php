6035adec0956f5b5b13278ff72938fb6ce70d5e7
Petr <pitfor@gmail.com>
2020-01-12 22:13:29 +0300

    Implement delete function for the question

--- app/Question.php
+++ app/Question.php
@@ -19,7 +19,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return route("questions.show", $this->id);
+        return route("questions.show", $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -36,6 +36,8 @@ class Question extends Model
         }
         return "unasnswered";
     }
-//    $question = Question::find(1);
 
+    public function getBodyHtmlAttribute(){
+        return \Parsedown::instance()->text($this->body);
+    }
 }
